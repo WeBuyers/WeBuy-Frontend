4c90524e33db9b7381cf4c1f4812b21a8dad63d9
OswaldHe <zifanhe1202@ucla.edu>
2020-05-16 16:12:09 -0700

    change styles and add model for wishlist

--- components/SearchPage.js
+++ components/SearchPage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Text, Item, Input, Container, Content, Card, CardItem, Right, Thumbnail} from 'native-base';
-import {Button, Icon} from "react-native-elements"
+import {Button, Icon, Overlay, ListItem} from "react-native-elements"
 import {View, StyleSheet, FlatList, ScrollView} from "react-native"
 import MapView from "react-native-maps";
 import {Marker, AnimatedRegion} from "react-native-maps";
@@ -18,7 +18,8 @@ class SearchPage extends Component {
             searchItem: "",
             stores: [],
             searchResult: [],
-            searched: false
+            searched: false,
+            modelvis: false
         }
         this.BottomRef = React.createRef();
     }
@@ -33,7 +34,7 @@ class SearchPage extends Component {
                     latitude: position.coords.latitude,
                     longitude: position.coords.longitude,
                 });
-                fetch(`${PLACE_API}/json?key=${API_KEY}&location=${position.coords.latitude},${position.coords.longitude}&radius=800&type=supermarket,grocery_or_supermarket`,{
+                fetch(`${PLACE_API}/json?key=${API_KEY}&location=${position.coords.latitude},${position.coords.longitude}&radius=800&type=supermarket`,{
                     method: "GET"
                 })
                     .then((response)=>{
@@ -143,12 +144,11 @@ class SearchPage extends Component {
                               borderRadius: 25
                           }}
                           >
-                              <CardItem>
+                              <CardItem cardBody={true}>
                                   <Button
                                       title={item.name}
-                                      color="lightblue"
-                                      size={50}
-                                      raised={true}
+                                      type="clear"
+                                      size={60}
                                       onPress={()=>{
                                           let newStores = this.state.stores;
                                           for (let i = 0; i < newStores.length; i++) {
@@ -164,7 +164,7 @@ class SearchPage extends Component {
                                   />
                               </CardItem>
                               <CardItem>
-                                  <Text style={{fontFamily: "Ubuntu-Regular"}}>{item.place}</Text>
+                                  <Text style={{fontFamily: "Ubuntu-Regular", fontSize: 15}}>{item.place}</Text>
                               </CardItem>
                           </Card>
                       )}
@@ -226,23 +226,21 @@ class SearchPage extends Component {
                         containerStyle={styles.autocompleteContainer}
                         listStyle={{borderColor: "transparent", backgroundColor: "transparent"}}
                         inputContainerStyle={{width: wp("65%"), borderColor: "white"}}
-                        listContainerStyle={{width: wp("65%"), backgroundColor: "transparent", elevation:1, borderColor: "transparent"}}
+                        listContainerStyle={{width: wp("65%"), backgroundColor: "transparent", elevation:1, borderColor: "transparent", marginTop: 5}}
                         renderItem={({item})=>(
                             <Item rounded style={{alignItems: 'center',
-                                borderRadius: 10,
+                                borderRadius: 3,
                                 height: hp("5%"),
                                 backgroundColor: '#fefefe',
-                                paddingBottom: 10,
-                                paddingTop: 5,
-                                marginTop: 10,
-                                justifyContent: "center"
+                                borderColor: "lightgrey",
+                                flex: 1
                             }}
                                   onPress={() => {
                                       this.setState({ searchItem: item });
 
                                   }}
                             >
-                                <Text>
+                                <Text style={{fontFamily: 'Jost', fontSize: 15, paddingLeft: 10}}>
                                     {item}
                                 </Text>
                             </Item>
@@ -281,7 +279,10 @@ class SearchPage extends Component {
                         borderRadius: 100,
                         shadowRadius: 30
                     }}
+                    onPress={()=>{this.setState({modelvis: !this.state.modelvis})}}
                 />
+
+
 
                 <BottomSheet
                     snapPoints={[700, 400, 100]}
@@ -290,6 +291,14 @@ class SearchPage extends Component {
                     renderContent={this.renderDrawer}
                     borderRadius={20}
                 />
+
+                <Overlay isVisible={this.state.modelvis} onBackdropPress={()=>{this.setState({modelvis: !this.state.modelvis})}}>
+                    <Text style={{fontSize: 18, alignSelf: 'center', paddingTop: 10}}> Import Your Wish List </Text>
+                    <View style={styles.modelContainer}>
+                        <Text> Here is some wish lists</Text>
+                    </View>
+                    <Button type="solid" size={30} title="Confirm"/>
+                </Overlay>
 
 
                 {/*<View style={{marginTop: hp("65%")}}>*/}
@@ -408,6 +417,11 @@ const styles = StyleSheet.create({
         marginLeft: 5,
         marginRight: 10,
     },
+    modelContainer: {
+        height: hp("40%"),
+        width: wp("70%"),
+        padding: 20
+    }
 });
 
 export default SearchPage;
