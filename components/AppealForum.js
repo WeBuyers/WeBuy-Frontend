6861f8227181b5a46656d841a622ad48f5b412f9
OswaldHe <zifanhe1202@ucla.edu>
2020-05-30 22:26:50 -0700

    set up forum style

--- components/AppealForum.js
+++ components/AppealForum.js
@@ -1,13 +1,75 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from "react-native";
-import {Container, Content, Text} from "native-base"
+import {View, StyleSheet, ScrollView} from "react-native";
+import {Container, Content, Text, List, ListItem, Header, Button, Icon, Right, Body} from "native-base"
+import {heightPercentageToDP as hp, widthPercentageToDP as wp} from "react-native-responsive-screen"
 
 class AppealForum extends Component {
     render() {
         return (
             <Container style={styles.container}>
                 <Content>
-                    <Text style={{paddingTop: 100, alignSelf: 'center'}}> This is forum page! </Text>
+                    <Header>
+                        <Text style={{fontSize: 18}}>Forum</Text>
+                    </Header>
+                    <ScrollView style={styles.scroll}>
+                        <List>
+                            <ListItem>
+                                <Body>
+                                    <Text> Topic 1</Text>
+                                </Body>
+                                <Right>
+                                    <Button transparent>
+                                        <Icon active name="thumbs-up" />
+                                        <Text>12 Likes</Text>
+                                    </Button>
+                                </Right>
+                            </ListItem>
+                            <ListItem>
+                                <Body>
+                                    <Text> Topic 2</Text>
+                                </Body>
+                                <Right>
+                                    <Button transparent>
+                                        <Icon active name="thumbs-up" />
+                                        <Text>12 Likes</Text>
+                                    </Button>
+                                </Right>
+                            </ListItem>
+                            <ListItem>
+                                <Body>
+                                    <Text> Topic 3</Text>
+                                </Body>
+                                <Right>
+                                    <Button transparent>
+                                        <Icon active name="thumbs-up" />
+                                        <Text>12 Likes</Text>
+                                    </Button>
+                                </Right>
+                            </ListItem>
+                            <ListItem>
+                                <Body>
+                                    <Text> Topic 4</Text>
+                                </Body>
+                                <Right>
+                                    <Button transparent>
+                                        <Icon active name="thumbs-up" />
+                                        <Text>12 Likes</Text>
+                                    </Button>
+                                </Right>
+                            </ListItem>
+                            <ListItem>
+                                <Body>
+                                    <Text> Topic 5</Text>
+                                </Body>
+                                <Right>
+                                    <Button transparent>
+                                        <Icon active name="thumbs-up" />
+                                        <Text>12 Likes</Text>
+                                    </Button>
+                                </Right>
+                            </ListItem>
+                        </List>
+                    </ScrollView>
                 </Content>
             </Container>
         );
@@ -21,6 +83,10 @@ const styles = StyleSheet.create({
         top: 0,
         bottom: 0,
         right: 0
+    },
+    scroll: {
+        height: hp("75%"),
+        margin: 10
     }
 });
 
